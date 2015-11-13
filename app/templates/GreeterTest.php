e1d7235070962acf070cb467d4d3eaa9eb3dcb60
Julián Gutiérrez <juliangut@gmail.com>
2015-11-13 18:49:36 +0100

    page docblock

--- app/templates/GreeterTest.php
+++ app/templates/GreeterTest.php
@@ -1,4 +1,14 @@
 <?php
+<% if (project.src || project.homepage) { -%>
+/**
+ * <%= project.desc %><% if (project.homepage) { -%> (<%= project.homepage %>)<% } -%>
+
+<% if (project.src) { -%>
+ *
+ * @link <%= project.src %> for the canonical source repository
+<% } -%>
+ */
+<% } -%>
 
 namespace Jgut\Generator\BarePHP\Tests;
 
