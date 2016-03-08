c7d81a9e29e6c875be0460fad6b3d8bafae6b516
Julián Gutiérrez <juliangut@gmail.com>
2016-03-08 21:18:04 +0100

    add license notice

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
+/*
+ * generator-barephp
+ * https://github.com/juliangut/generator-barephp
+ *
+ * Copyright (c) 2016 Julián Gutiérrez (juliangut@gmail.com)
+ * Licensed under the BSD-3-Clause license.
+ */
+
 'use strict';
 
 module.exports = function(grunt) {
--- test/barephp.js
+++ test/barephp.js
@@ -1,3 +1,10 @@
+/*
+ * generator-barephp
+ * https://github.com/juliangut/generator-barephp
+ *
+ * Copyright (c) 2016 Julián Gutiérrez (juliangut@gmail.com)
+ * Licensed under the BSD-3-Clause license.
+ */
 
 'use strict';
 
